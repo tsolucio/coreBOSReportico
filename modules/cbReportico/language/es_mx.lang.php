533451c05b9da913091c37815562ee266277f1c2
Omar Llorens <omar@tsolucio.com>
2014-11-25 13:19:29 +0100

    add es_mx language

---
+++ modules/cbReportico/language/es_mx.lang.php
@@ -0,0 +1,34 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'cbReportico' => 'Reportico',
+	'SINGLE_cbReportico' => 'Reportico',
+	'cbReportico ID' => 'Reportico ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Información Personalizada',
+	'LBL_CBREPORTICO_INFORMATION' => 'Datos Informe Reportico',
+        'LBL_DESCRIPTION_INFORMATION' => 'Descripción',
+
+	'REPORTICO_NO' => 'Nº Informe',
+        'Report Name' => 'Nombre del Informe',
+        'Project Name' => 'Nombre del Proyecto',
+        'Execute Mode' => 'Modo de Ejecución',
+        'Output Format By Default' => 'Formato de salida por defecto',
+        'Show Graph' => 'Mostrar Gráfico',
+        'Show Group Headers' => 'Mostrar Cabeceras de los Grupos',
+        'Show Group Trailers' => 'Show Group Trailers',
+        'Show Columns Headers' => 'Mostrar Cabeceras de las Columnas',
+        'Show Criteria' => 'Mostrar Criterios',
+        'VIEW_REPORT' => 'Ver Informe',
+        'View Report' => 'Ver Informe',
+);
+
+?>
